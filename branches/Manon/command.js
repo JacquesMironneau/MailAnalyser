40fadb449cd7e26ee370402f55347ddca98f6721
caroyman <caroyman@62c95f42-5e80-46b0-960d-239804a02b83>
2020-11-04 08:39:21 +0000

    Ajout de la couleur cyan

--- branches/Manon/command.js
+++ branches/Manon/command.js
@@ -1,6 +1,7 @@
 const Command = {
 	check: function() {
 	console.log("Command ready")
+	console.log("Cyan level checked".cyan)
 	return true
 	} 
 }
--- branches/Manon/radio.js
+++ branches/Manon/radio.js
@@ -1,6 +1,7 @@
 const Radio = {
 	check: function () {
 		console.log("Radio ready")
+		console.log("Cyan level checked".cyan)
 		return true
 	} 
 }
