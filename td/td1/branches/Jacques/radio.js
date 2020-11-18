4e3da5954c28801bedbdb5a56b39ebf676c76955
pashmi <pashmi@62c95f42-5e80-46b0-960d-239804a02b83>
2020-11-18 08:00:10 +0000

    merge branch jacques

--- td/td1/branches/Jacques/radio.js
+++ td/td1/branches/Jacques/radio.js
@@ -1,8 +1,11 @@
 const Radio = {
 	check: function () {
-		console.log("Radio ready")
+		console.log("Radio ready");
+		console.log("Blue level checked".blue);
+		console.log("Magenta level checked".magenta)
+		console.log("Cyan level checked".cyan)
 		console.log("Magenta level checked".magenta)
 		return true
-	} 
+	}
 }
-module.exports = Radio
+module.exports = Radio 
