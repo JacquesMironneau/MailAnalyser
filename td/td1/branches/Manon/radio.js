6231d91394ad316f0c35bd2cdec9467053024725
pashmi <pashmi@62c95f42-5e80-46b0-960d-239804a02b83>
2020-11-18 08:33:46 +0000

    retour vers le futur

---
+++ td/td1/branches/Manon/radio.js
@@ -0,0 +1,10 @@
+const Radio = {
+	check: function () {
+		console.log("Radio ready");
+		console.log("Blue level checked".blue);
+		console.log("Magenta level checked".magenta)
+		console.log("Cyan level checked".cyan)
+		return true
+	} 
+}
+module.exports = Radio
