d3f4cf7207a7a62d03e78cdb7ba4165d8f17ac81
caroyman <caroyman@62c95f42-5e80-46b0-960d-239804a02b83>
2020-11-18 07:44:58 +0000

    merge branche Manon

--- td/td1/trunk/command.js
+++ td/td1/trunk/command.js
@@ -3,6 +3,7 @@ const Command = {
 		console.log("Command ready")
 		console.log("Blue level checked".blue)
 		console.log("Magenta level checked".magenta)
+		console.log("Cyan level checked".cyan)
 		return true
 	} 
 }
--- td/td1/trunk/radio.js
+++ td/td1/trunk/radio.js
@@ -3,6 +3,7 @@ const Radio = {
 		console.log("Radio ready");
 		console.log("Blue level checked".blue);
 		console.log("Magenta level checked".magenta)
+		console.log("Cyan level checked".cyan)
 		return true
 	} 
 }
