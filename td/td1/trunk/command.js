feaade9110847ad6aa83c3c37efa4eb58688de31
pashmi <pashmi@62c95f42-5e80-46b0-960d-239804a02b83>
2020-11-18 07:30:58 +0000

    fix color name

--- td/td1/trunk/command.js
+++ td/td1/trunk/command.js
@@ -1,8 +1,8 @@
 const Command = {
 	check: function() {
 		console.log("Command ready")
+		console.log("Blue level checked".blue)
 		console.log("Magenta level checked".magenta)
-		console.log("Magenta level checked".blue)
 		return true
 	} 
 }
--- td/td1/trunk/radio.js
+++ td/td1/trunk/radio.js
@@ -1,7 +1,7 @@
 const Radio = {
 	check: function () {
 		console.log("Radio ready");
-		console.log("blue level checked".blue);
+		console.log("Blue level checked".blue);
 		console.log("Magenta level checked".magenta)
 		return true
 	} 
