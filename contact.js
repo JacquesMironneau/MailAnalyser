653259d0eac15c3b000794871fdec95a6bdfd44b
Jacques Mironneau <34798699+JacquesMironneau@users.noreply.github.com>
2020-11-30 10:35:46 +0000

    Vcard display done, add package-json

--- contact.js
+++ contact.js
@@ -4,19 +4,23 @@
  */
 class Contact
 {
+  constructor(name, lastname, mail)
+  {
+    this.name = name;
+    this.lastname = lastname;
+    this.mail = mail;
+  }
 
-    constructor(name,lastname,mail)
-    {
-        this.name = name
-        this.lastname = lastname
-        this.mail = mail
-    }
-
-    // Display the contact to a vcard format.
-    toVcard()
-    {
-        console.log("Not implemented yet");
-    }
+  // Display the contact to a vcard format.
+  toVcard()
+  {
+    let vcardContact = 'BEGIN:VCARD\nVERSION:4.0\n';
+    vcardContact += `N:${this.lastname};${this.name};;;\n`;
+    vcardContact += `FN:${this.name} ${this.lastname}\n`;
+    vcardContact += `EMAIL:${this.mail}\n`;
+    vcardContact += 'END:VCARD\n';
+    return vcardContact;
+  }
 }
 
-module.exports = { Contact }
+module.exports = { Contact };
