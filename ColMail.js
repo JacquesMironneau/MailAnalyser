fd0fe0d9ed53c57e052dcff24afda12e635e05d6
pertingens <pertingens@62c95f42-5e80-46b0-960d-239804a02b83>
2020-12-07 08:06:33 +0000

    ajout de methode dans mail et colmail

--- ColMail.js
+++ ColMail.js
@@ -2,7 +2,7 @@
     * Colection Mail class, represent an email collection.
     * @author Augustin Borne
 */
-const mail = require('./Mail.js');
+const {Mail} = require('./Mail.js');
 
 class ColMail{ 
     constructor(nomCollection){
@@ -10,9 +10,11 @@ class ColMail{
         this.listeMail = [];
     }
 
-    setListeMail(mail){
-        if(mail instanceof Mail){
-            this.listeMail.push(mail);
+    setListeMail(mailEntre){
+        if(mailEntre instanceof Mail){
+            this.listeMail.push(mailEntre);
+        }else{
+            throw Error('Invalid data type, a Mail element is required')
         }
         
     }
@@ -21,6 +23,67 @@ class ColMail{
         let res = "colection " + this.nomCollection+" :";
         this.listeMail.forEach(element => res+= "\n" +element.toString);
         return res;
+    }
+
+    mailInInterval(date1,date2){
+        result = [];
+    }
+
+    bestCollab(email){
+        result =[];
+    }
+    bestCollab(nom,prenom){
+        result =[];
+    }
+
+    MostUsedTerm(email){
+        result=[];
+    }
+
+    MostUsedTerm(nom,prenom){
+        result=[];
+    }
+
+    SearchByEmail(email){
+        result=[];
+        this.listeMail.forEach(element => {
+            if(element instanceof Mail){
+                if(element.getEmailAuthor === email || element.getEmailReceiver === email){
+                    result.push(element);
+                }
+            }else{
+                throw Error('Invalid data type, a Mail element is required')
+            } 
+        });
+        return result;
+    }
+    SearchByAuthor(nom,prenom){
+        result=[];
+        const regexNom = new RegExp(nom);
+        const regexPrenom = new RegExp(prenom);
+        this.listeMail.forEach(element => {
+            if(element instanceof Mail){
+                if(regexNom.test(element.getAuthor) && regexPrenom.test(element.getAuthor)){
+                    result.push(element);
+                }
+            }else{
+                throw Error('Invalid data type, a Mail element is required')
+            } 
+        });
+        return result;
+    }
+
+    getMail(messageId){
+        this.listeMail.forEach(element => {
+            if(element instanceof Mail){
+                if(element.getMessageId === messageId){
+                    return element;
+                }
+            }else{
+                throw Error('Invalid data type, a Mail element is required')
+            }            
+        });
+        return "pas d'email correspondant a"+messageId;
     }
 
 }
