561ff7117b8d3f5edaa0257fe6706c13cb242ddc
pertingens <pertingens@62c95f42-5e80-46b0-960d-239804a02b83>
2020-12-10 20:18:15 +0000

    ajout de la classe nbUseTerm

---
+++ NbUseTerm.js
@@ -0,0 +1,37 @@
+/**
+    * NbUseTerm class, represent a relation between two contact.
+    * @author Augustin Borne
+*/
+
+class NbUseTerm{
+    constructor(term,nbUse){
+        this.term=term;
+        this.nbUse=nbUse;
+    }
+
+    get getTerm(){
+        return this.term;
+    }
+
+    get getNbUse(){
+        return this.nbUse;
+    }
+
+    setTerm(term){
+        this.term=term;
+    }
+
+    setNbUse(nb){
+        this.nbUse=nb;
+    }
+
+    addNbUse(){
+        this.term++;
+    }
+
+    minNbUse(){
+        this.term--;
+    }
+}
+
+module.exports = { NbUseTerm };
