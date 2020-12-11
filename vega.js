1a274a5477463e116acdd5c67fc21e9b0b071f82
pertingens <pertingens@62c95f42-5e80-46b0-960d-239804a02b83>
2020-12-11 07:25:50 +0000

    ajout des modifs de jacques (vega)

--- vega.js
+++ vega.js
@@ -1,42 +1,76 @@
+/* eslint-disable */
 /**
     * vega lite module, export to png or svg.
-    * @author Augustin Borne
+    * @author Augustin Borne, Jacques Mironneau
 */
-
-const {Mail} = require('./Mail.js');
-const { Contact } = require('./contact');
-const {ColMail} = require('./ColMail.js');
-const fs = require('fs');
-const vegaCli = require('vega-cli');
-const vegalite = require('vega-lite');
+const colors = require('colors');
 const vega = require('vega');
-
-function top10Interloc(listeContact){
-    let top10 = '{\n\"$schema\": \"https://vega.github.io/schema/vega-lite/v4.json\",\n\"data\": {\n\"values\": [\n'
-    listeContact.forEach(element => {
-        if(element instanceof Contact){
-            top10+='{\"Nom\": \"'+element.lastName+'\", \"Prénom\": \"'+element.getName+'\", \"E-mail\": \"'+element.getMail+'\"},\n'
-        }else{
-            throw Error('Invalid data type, a Contact element is required')
-        }
-    });
-    top10+=']\n}\n}'
-    fs.writeFile('result.json',top10,function(erreur){
-        if(erreur){
-            console.log(erreur)
-        }
-    })
-
-   
-}
-
-function toSVGchart(vlSpec,filename){
-    const myChart = vegalite.compile(vlSpec).spec;
-
-    const runtime =vega.parse(myChart);
-    const view = new vega.View(runtime).renderer('svg').run;
-    const mySvg = view.toSVGchart;
-    view.
-}
+const vl = require('vega-lite');
+const fs = require('fs');
 
 
+
+const render = (spec, format) =>
+{
+  const vgSpec = vl.compile(spec).spec;
+  const view = new vega.View(vega.parse(vgSpec))
+    .renderer('none')
+    .initialize();
+  // generate a static SVG image
+  if (format === 'svg')
+  {
+    const fileName = 'test.svg';
+    view.toSVG()
+      .then((svg) =>
+      {
+        fs.writeFileSync(fileName, svg, (err) =>
+        {
+          if (err)
+          {
+            console.err(err);
+          }
+        });
+        console.log(`${'[OK]'.green.bold} Graphic rendered to ${fileName}`);
+      })
+      .catch((err) =>
+      {
+        console.error(err);
+      });
+  }
+};
+
+const visualInteraction = (interactionList, format) => 
+{
+
+  const people = [];
+  interactionList.forEach((inter) => {
+    if (!people.includes(inter.getContact1.getMail))
+      people.push(inter.getContact1.getMail)
+
+    if (!people.includes(inter.getContact2.getMail))
+      people.push(inter.getContact2.getMail)
+
+  });
+  console.log(people)
+
+  const spec = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v3.json',
+    data: { url: 'data.json' },
+    mark: {
+      type: 'circle',
+    },
+    encoding: {
+      // ne marche pas avec people[0] ou 1, les fields doivent provenir de data.json :c
+      x: { field: people[0], type: 'nominal', axis: { title: 'a' } },
+  
+      y: { field: people[1], type: 'nominal', axis: { title: 'b' } },
+    },
+  };
+
+  render(spec, format);
+
+};
+
+
+
+module.exports = { visualInteraction };
