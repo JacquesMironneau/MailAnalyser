00f6ac4a6de69526c04329fc7bd693101c29e2e4
pashmi <pashmi@62c95f42-5e80-46b0-960d-239804a02b83>
2020-12-11 15:31:24 +0000

    vega works for top10, add png render, fix cli bug

--- branches/command/vega.js
+++ branches/command/vega.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 /**
     * vega lite module, export to png or svg.
-    * @author Augustin Borne, Jacques Mironneau
+    * @author Jacques Mironneau
 */
 const colors = require('colors');
 const vega = require('vega');
@@ -9,20 +9,26 @@ const vl = require('vega-lite');
 const fs = require('fs');
 
 
-
+/**
+ * 
+ * @param {json vega-lite spec} spec The vegalite spec to export as a file
+ * @param {['png','svg']} format format of the exported graphic
+ */
 const render = (spec, format) =>
 {
+  // Compile vegalite spec to vega spec
   const vgSpec = vl.compile(spec).spec;
   const view = new vega.View(vega.parse(vgSpec))
     .renderer('none')
     .initialize();
-  // generate a static SVG image
+
   if (format === 'svg')
   {
     const fileName = 'test.svg';
     view.toSVG()
       .then((svg) =>
       {
+        // Write the svg string into a file
         fs.writeFileSync(fileName, svg, (err) =>
         {
           if (err)
@@ -36,9 +42,20 @@ const render = (spec, format) =>
       {
         console.error(err);
       });
+  } else
+  {
+    view.toCanvas()
+      .then((canvas) => {
+        const fileName = "test.png"
+        const stream = canvas.createPNGStream();
+        const out = fs.createWriteStream(fileName);
+        stream.pipe(out);
+        out.on('finish', () => console.log(`${'[OK]'.green.bold} Graphic rendered to ${fileName}`));
+
+      })
   }
 };
-
+// Work in progress
 const visualInteraction = (interactionList, format) => 
 {
 
@@ -71,6 +88,48 @@ const visualInteraction = (interactionList, format) =>
 
 };
 
+const top10Interloc = (data, format) =>
+{
+   const spec = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+    "data": { "values": data},
+    "mark": "bar",
+    "title": "Top 10 interlocteurs pour " + data[0].contact1.mail,
+    "encoding": {
+      "x": {
+        "field": "contact2.mail",
+        "title": "Interlocuteurs",
+        "sort": "-y"
+      },
+      "y": {
+        "field": "nbEchange",
+        "type": "quantitative",
+        "title": "Nombre d'échanges"}
+      }
+  };
+  render(spec,format);
+}
 
+const top10term = (data, format) =>
+{
+  const spec = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+    "data": { "values": data},
+    "mark": "bar",
+    "title": "Top 10 des termes utilisés dans les objets de mails",
+    "encoding": {
+      "x": {
+        "field": "term",
+        "title": "Termes utilisés",
+        "sort": "-y"
+      },
+      "y": {
+        "field": "nbUse",
+        "type": "quantitative",
+        "title": "Occurrence d'un terme"}
+      }
+  };
+  render(spec,format)
+}
 
-module.exports = { visualInteraction };
+module.exports = { visualInteraction, top10Interloc, top10term };
